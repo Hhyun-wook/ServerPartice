096f85f11b93eb62dadab365323ae303b81dce8d
HyunWook <sigh2879@gmail.com>
2023-09-25 16:59:10 +0900

    11_CPU파이프라인_개론
    컴파일러 또는 CPU에서 성능 향상을 위해 명령어를 뒤집어서 실행 할 수 있다.

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,48 +6,65 @@
 #include <mutex>  // 리눅스든 윈도우든 상관없음
 #include <future> // 리눅스든 윈도우든 상관없음
 #include <Windows.h>
-int32 buffer[10000][10000];
+
+ //1) 가시성 , 2) 코드 재배치 란 문제가 있다.
+ //1로 대입하더라도 1로 나오지 않을 수 있다.-> 가시성
+ //코드 재배치 -> 컴파일러가 최적화할 때 재배치할 수 있다.
+	//		  -> 또는 CPU가 재배치할 수 있다.
+	//		  -> 싱글쓰레드에서는 문제가 없을 수 있으나 멀티쓰레드에서는 다른 쓰레드에서 건들 수 있기 때문에 이상해진다.
+	//		
+
+int32 x = 0;
+int32 y = 0;
+int32 r1 = 0;
+int32 r2 = 0;
+
+volatile bool ready =false;
+
+void Thread_1()
+{
+	while (!ready)
+		;
+
+	y = 1;  //store y		
+	r1 = x; // Load X
+}
+
+void Thread_2()
+{
+	while (!ready)
+		;
+
+	x = 1;  //store x
+	r2 = y; // Load y
+}
 
 
 
 int main()
 {
-	memset(buffer, 0, sizeof(buffer));
+	int32 count = 0;
 
+	while (true)
 	{
-		int64 sum = 0;
+		ready = false;
+		count++;
 
-		uint64 start = GetTickCount64();
+		x = y = r1 = r2 = 0;
 
-		for (int32 i = 0; i < 10000; ++i)
-		{
-			for (int32 j = 0; j < 10000; ++j)
-				sum += buffer[i][j];
-		}
+		thread t1(Thread_1);
+		thread t2(Thread_2);
 
-		uint64 end = GetTickCount64();
-		cout << "Elasped_Tick" << end - start << endl;
+		t1.join();
+		t2.join();
+
+		if (r1 == 0 && r2 == 0)
+			break;
+
 	}
 
 
-	{
-		int64 sum = 0;
-
-		uint64 start = GetTickCount64();
-
-		for (int32 i = 0; i < 10000; ++i)
-		{
-			for (int32 j = 0; j < 10000; ++j)
-				sum += buffer[j][i];
-		}
-
-		uint64 end = GetTickCount64();
-		cout << "Elasped_Tick" << end - start << endl;
-	}
-	// 거의 3배이상의 차이가 난다.
-	// 공간지역성 + 시간지역성의 원리로 작성된 캐시 철학 때문이다.
-
-
+	cout << count << " 만에 빠져나옴" << endl;
 
 }
 
