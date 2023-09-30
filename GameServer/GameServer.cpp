9a2d64b405da89b505142fd99a22636f66df893e
HyunWook <sigh2879@gmail.com>
2023-09-30 13:01:17 +0900

    2_1_래퍼랜스카운팅_직접만들기
    수동적으로 만든 래퍼랜스 카운트가 있으면 객체포인터를 복사 후에 래퍼랜스 카운트를 올리기전 중간단계에서 해당 객체가 삭제 되는 경우가 있을 수있다. 이 경우 복사한 객체가 댕글리 포인터가 될 가능성이 있으므로 스마트포인터를 사용해야한다.

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,70 +7,93 @@
 #include <future> // 리눅스든 윈도우든 상관없음
 #include <Windows.h>
 #include "ThreadManager.h"
+#include "RefCounting.h"
 
-// 소수 구하기
-
-// 1과 자기 자신으로만 나뉘면 그것을 소수라고함
-
-bool IsPrime(int number)
+class Wraight : public RefCountable
 {
-	if (number <= 1)
+public:
+
+
+public:
+	int _hp = 150;
+	int _posX = 0;
+	int _posY = 0;
+
+};
+
+using WraightRef = TSharedPtr<Wraight>;
+
+class Missile : public RefCountable
+{
+public:
+	
+	void SetTarget(WraightRef target)
+	{ 
+		// 싱글스레드에서 상관없지만 멀티쓰레드에서는 문제가 된다.
+		_target = target;
+		// 중간에 개입 가능하다.(멀티쓰레드에서 문제가 생김)
+		//target->AddRef();  // 래퍼랜스 카운트를 증가시키는 것이랑
+	}
+
+	void Test(WraightRef& target)
+	{
+		// 복사비용이 아쉬우면 참조를 사용하면 래퍼랜스가 늘어나지않는다.
+		// 그래서 잠시만 사용할거면 참조를 이용해서 사용하면 래퍼랜스카운트가 증가하지않는다.
+	}
+
+
+	bool Update()
+	{
+		if (_target == nullptr)
+			return true;
+
+		int PosX = _target->_posX;
+		int PosY = _target->_posY;
+	
+		if (_target->_hp == 0)
+		{
+			_target->ReleaseRef();
+			_target = nullptr;
+			return true;
+		}
+
 		return false;
-	if (number == 2 || number == 3)
-		return true;
-
-	for (int i = 2; i < number; ++i)
-	{
-		if ((number % i) == 0)
-			return false;
 	}
 
-	return true;
-}
+	Wraight* _target = nullptr;
+	
 
-//[start,end]
-int CountPrime(int start, int end)
-{
-	int count = 0;
+};
 
-	for (int i = start; i <= end; ++i)
-	{
-		if (IsPrime(i))
-			++count;
-	}
-
-	return count;
-}
+using MissileRef = TSharedPtr<Missile>;
 
 int main()
 {
-	const int MAX_NUMBER = 100'000;
+	WraightRef wraight(new Wraight());
+	wraight->ReleaseRef();
+	
+	MissileRef missile(new Missile());
+	missile->ReleaseRef();
 
-	// 1 ~ MAX_NUMBER 까지의 소수 개수
 
-	vector<thread> threads;
+	
 
-	int coreCount = thread::hardware_concurrency();
-	int jobCount = (MAX_NUMBER / coreCount)+1;
+	missile->SetTarget(wraight);
+		
+	wraight->_hp = 0;
+	//wraight->ReleaseRef();
+	wraight = nullptr;
 
-	atomic<int32> primeCount = 0;
-
-	for (int i = 0; i < coreCount; ++i)
+	while (true)
 	{
-		int start = (i * jobCount) + 1;
-		int end = min(MAX_NUMBER, ((i + 1) * jobCount));
+		if (missile->Update())
+		{
+			missile->ReleaseRef();
+			missile = nullptr;
 
-		threads.push_back(thread([start, end, &primeCount]()
-			{
-				primeCount += CountPrime(start, end);
-			}));
+		}
 	}
-
-	for (thread &t : threads)
-	{
-		t.join();
-	}
-
-	cout << primeCount << endl;
+	
+	
 
 }
