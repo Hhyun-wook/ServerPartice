35341c97ffdedce328d831d84876cf82c19b0719
HyunWook <sigh2879@gmail.com>
2023-10-04 21:57:10 +0900

    3_Server_setting_(TCP_Socket)
    게임서버 프로젝트 클라이언트 프로젝트 서버 연동

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -1,8 +1,82 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib") // 서버를 사용할려면 필요하다.
+
 int main()
 {
+
+	// 윈속 초기화(ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	// 기본적으로 한국 게임은 TCP이다.
+	// TCP는 통신을 UDP보다 안전하게 해준다.
+
+
+	// af : address Family (AF_INET = IPv4 , AF_INET6 = IPv6)
+	// type : TCP(SOCK_STREAM, vs UDP(SOCK_DGRAM)
+	// protocol : descriptor
+	// return : descriptor
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);		// 소문자는 리눅스에서도 사용가능할 수 있음
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Sokcet ErrCode : " << errCode <<endl;
+		return 0;
+	}
+
+	// 연결할 목적지는? (IP 주소 + port) -> xx 아파트 yy호
+	SOCKADDR_IN serverAddr; // IPv4 버젼
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1"); 구식버전
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // 현대식 버전
+	serverAddr.sin_port = ::htons(7777);  // 80 : HTTP 특정번호는 규약이 있다.
+
+	// htons 
+	// host to networt short(long 도있다.) 
+	// Little-Endian vs Big-Endian
+	// ex) 0x12345678 4바이트 정수
+	// low [0x78][0x56][0x34][0x12] high   little 앤디안
+	// low [0x12][0x34][0x56][0x78] high   big    앤디안 -> network는 빅앤디안규약이다.
+	// 왠만하면 리틀앤디안 사용한다. 나는 리틀앤디안으로 하는데 상대통신은 빅앤디안을 사용한다?
+	// 문제발생 
+	// 이러한 빅앤디안 리틀앤디안을 맞춰주기위해 htons 함수를 사용한다.
+	// 반대 버전의 함수도 있다. 
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Sokcet ErrCode : " << errCode << endl;
+		return 0;
+	}
+
+	// -------------------------
+	// 연결 성공 : 이제부터 데이터 송수신 가능!
+
+	cout << "Connected To Server ! " << endl;
+
+	while (true)
+	{
+		//ToDo
+
+		this_thread::sleep_for(1s);
+	}
+
+
+	// ----------------------------
+
+	// 소켓 리소스 반환
+	::closesocket(clientSocket);
+
+	// 윈속 종료
+	::WSACleanup();
+
 
 }
 
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,16 +1,19 @@
-﻿#include "pch.h"
-#include <iostream>
-#include "Corepch.h"
-#include <thread> // 리눅스든 윈도우든 상관없음
-#include <atomic> // 리눅스든 윈도우든 상관없음
-#include <mutex>  // 리눅스든 윈도우든 상관없음
-#include <future> // 리눅스든 윈도우든 상관없음
-#include <Windows.h>
+﻿//#include "pch.h"
+//#include <iostream>
+//#include "Corepch.h"
+//#include <thread> // 리눅스든 윈도우든 상관없음
+//#include <atomic> // 리눅스든 윈도우든 상관없음
+//#include <mutex>  // 리눅스든 윈도우든 상관없음
+//#include <future> // 리눅스든 윈도우든 상관없음
+//#include <Windows.h>
+//#include "ThreadManager.h"
+//
+//
+//#include <winsock2.h>
+//#include <mswsock.h>
+//#include <ws2tcpip.h>
+//#pragma comment(lib, "ws2_32.lib")
 
-#include "ThreadManager.h"
-#include "RefCounting.h"
-#include "Memory.h"
-#include "Allocator.h"
 
 //
 //DECLSPEC_ALIGN(16)
@@ -74,102 +77,190 @@
 //
 //}
 
-enum PLAYER_TYPE
-{
-	KNIGHT,
-	MAGE,
+//enum PLAYER_TYPE
+//{
+//	KNIGHT,
+//	MAGE,
+//
+//};
+//
+//using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
+//
+//class Player
+//{
+//
+//
+//public:
+//	Player()
+//	{
+//		INIT_TL(Player);
+//	}
+//
+//	virtual ~Player(){}
+//	DECLEAR_TL
+//
+//
+//};
+//
+//class Knight :public Player
+//{
+//public:
+//	Knight() { INIT_TL(Knight); }
+//};
+//
+//
+//class Mage :public Player
+//{
+//public:
+//	Mage() { INIT_TL(Mage); }
+//};
+//
+//
+//class Archer :public Player
+//{
+//public:
+//	Archer() { INIT_TL(Archer); }
+//};
+//
+//
+//class Monster
+//{
+//public:
+//	int64 _id = 0;
+//};
+//
+//int main()
+//{
+//	//Knight* k = ObjectPool<Knight>::Pop();
+//	//ObjectPool<Knight>::Push(k);
+//	// shared_ptr<Knight>  sptr = make_shared<Knight>();
+//	// 이런식으로하면 기본적인 New delete를 이용한다.
+//	// 그래서 메모리,오브젝트풀링시 이런식으로 만들면안된다.
+//
+//	//Knight* knights[100];
+//
+//	//for (int32 i = 0; i < 100; ++i)
+//	//{
+//	//	knights[i] = ObjectPool<Knight>::Pop();
+//	//}
+//
+//
+//	//for (int32 i = 0; i < 100; ++i)
+//	//{
+//	//	 ObjectPool<Knight>::Push(knights[i]);
+//	//	 knights[i] = nullptr;
+//	//}
+//
+//	//shared_ptr<Knight> sptr = { MakeShared<Knight>() };
+//
+//
+//	//{
+//	//	Player* player = new Player();
+//
+//	//	bool canCast = CanCast<Knight*>(player);
+//	//	Knight* knight = TypeCast<Knight*>(player);
+//
+//	//	
+//
+//	//	delete player;
+//	//}
+//
+//	//{
+//	//	shared_ptr<Knight> knight = MakeShared<Knight>();
+//
+//	//	shared_ptr<Player> player = TypeCast<Knight>(knight);
+//	//	bool canCast = CanCast<Player>(knight);
+//	//}
+//
+//
+//
+//
+//}
 
-};
+#include "pch.h"
+#include <iostream>
+#include "CorePch.h"
+#include <atomic>
+#include <mutex>
+#include <windows.h>
+#include <future>
+#include "ThreadManager.h"
 
-using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
-
-class Player
-{
-
-
-public:
-	Player()
-	{
-		INIT_TL(Player);
-	}
-
-	virtual ~Player(){}
-	DECLEAR_TL
-
-
-};
-
-class Knight :public Player
-{
-public:
-	Knight() { INIT_TL(Knight); }
-};
-
-
-class Mage :public Player
-{
-public:
-	Mage() { INIT_TL(Mage); }
-};
-
-
-class Archer :public Player
-{
-public:
-	Archer() { INIT_TL(Archer); }
-};
-
-
-class Monster
-{
-public:
-	int64 _id = 0;
-};
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	//Knight* k = ObjectPool<Knight>::Pop();
-	//ObjectPool<Knight>::Push(k);
-	// shared_ptr<Knight>  sptr = make_shared<Knight>();
-	// 이런식으로하면 기본적인 New delete를 이용한다.
-	// 그래서 메모리,오브젝트풀링시 이런식으로 만들면안된다.
+	// 윈속 초기화 (ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-	//Knight* knights[100];
-
-	//for (int32 i = 0; i < 100; ++i)
-	//{
-	//	knights[i] = ObjectPool<Knight>::Pop();
-	//}
-
-
-	//for (int32 i = 0; i < 100; ++i)
-	//{
-	//	 ObjectPool<Knight>::Push(knights[i]);
-	//	 knights[i] = nullptr;
-	//}
-
-	//shared_ptr<Knight> sptr = { MakeShared<Knight>() };
-
-
+	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+	// protocol : 0
+	// return : descriptor
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
 	{
-		Player* player = new Player();
-
-		bool canCast = CanCast<Knight*>(player);
-		Knight* knight = TypeCast<Knight*>(player);
-
-		
-
-		delete player;
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
 	}
 
-	{
-		shared_ptr<Knight> knight = MakeShared<Knight>();
+	// 나의 주소는? (IP주소 + Port)->XX 아파트 YY 호
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); //< 니가 알아서 해줘
+	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
 
-		shared_ptr<Player> player = TypeCast<Knight>(knight);
-		bool canCast = CanCast<Player>(knight);
+	// 안내원 폰 개통! 식당의 대표 번호
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
 	}
 
+	// 영업 시작!
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// -----------------------------
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr; // IPv4
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		// 손님 입장!
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected! IP = " << ipAddress << endl;
+
+		// TODO
+	}
+
+	// -----------------------------
 
 
-
+	// 윈속 종료
+	::WSACleanup();
 }
