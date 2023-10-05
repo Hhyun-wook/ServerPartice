8989162d02fd85c0258553b754f6a336cf445f00
HyunWook <sigh2879@gmail.com>
2023-10-05 23:09:18 +0900

    3_3_SOCKET과_관련된_setsocketopt()_getsocketopt()함수의_옵션들

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -123,108 +123,132 @@ void HandleError(const char* cause)
 
 
 // UDP
+//int main()
+//{
+//
+//	// 윈속 초기화(ws2_32 라이브러리 초기화)
+//	// 관련 정보가 wsaData에 채워짐
+//	WSAData wsaData;
+//	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+//		return 0;
+//
+//	// 기본적으로 한국 게임은 TCP이다.
+//	// TCP는 통신을 UDP보다 안전하게 해준다.
+//
+//
+//	// af : address Family (AF_INET = IPv4 , AF_INET6 = IPv6)
+//	// type : TCP(SOCK_STREAM, vs UDP(SOCK_DGRAM)
+//	// protocol : descriptor
+//	// return : descriptor
+//	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);		// 소문자는 리눅스에서도 사용가능할 수 있음
+//	if (clientSocket == INVALID_SOCKET)
+//	{
+//		HandleError("Socket");
+//		return 0;
+//	}
+//
+//	// 연결할 목적지는? (IP 주소 + port) -> xx 아파트 yy호
+//	SOCKADDR_IN serverAddr; // IPv4 버젼
+//	::memset(&serverAddr, 0, sizeof(serverAddr));
+//	serverAddr.sin_family = AF_INET;
+//	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1"); 구식버전
+//	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // 현대식 버전
+//	serverAddr.sin_port = ::htons(7777);  // 80 : HTTP 특정번호는 규약이 있다.
+//	
+//	//udp 는 따로 연결이 필요없다.
+//
+//	// 하지만 즐겨찾기 처럼 내가 보낼 대상을 설정할 수 있다.
+//	::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+//
+//	// -------------------------
+//	// 연결 성공 : 이제부터 데이터 송수신 가능!
+//
+//	cout << "Connected To Server ! " << endl;
+//
+//	while (true)
+//	{
+//		//ToDo
+//		char sendBuffer[100] = "HelloServer";
+//
+//
+//		// 나의 IP 주소 + 포트 번호 설정 ( 자동 할당) TCP,UDP 상관없음)
+//		// Unconnect UDP (기본 UDP)
+//	   /*	int32 reslutCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0
+//			, (SOCKADDR*)&serverAddr, sizeof(serverAddr));*/
+//
+//		// Connect UDP
+//		
+//		for (int32 i = 0; i < 10; ++i)
+//		{
+//			int32 reslutCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+//			if (reslutCode == SOCKET_ERROR)
+//			{
+//				HandleError("SendTo");
+//				return 0;
+//			}
+//		}
+//
+//		
+//
+//
+//		
+//		cout << "Send Data Len : " << sizeof(sendBuffer) << endl;
+//
+//
+//		SOCKADDR_IN recvAddr;
+//		::memset(&recvAddr, 0, sizeof(recvAddr));
+//		int32 addrLen = sizeof(recvAddr);
+//		char recvBuffer[1000];
+//
+//		// Unconnect UDP (기본 UDP) 동작방식은 상관없음
+//		/*int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0,
+//								(SOCKADDR*)&recvAddr,&addrLen);*/
+//		// Connect UDP
+//		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+//
+//
+//		if (recvLen <= 0)
+//		{
+//			int32 errCode = ::WSAGetLastError();
+//			cout << "Accept ErrorCode : " << errCode << endl;
+//			return 0;
+//		}
+//
+//		cout << "Client Recv Data :  Data : " << recvBuffer << endl;
+//		cout << "Client Recv Data :  Len : " << recvLen << endl;
+//
+//
+//		this_thread::sleep_for(1s);
+//	}
+//
+//
+//	// ----------------------------
+//
+//	// 소켓 리소스 반환
+//	::closesocket(clientSocket);
+//
+//	// 윈속 종료
+//	::WSACleanup();
+//
+//
+//}
+
+
 int main()
 {
-
-	// 윈속 초기화(ws2_32 라이브러리 초기화)
+	// 윈속 초기화 (ws2_32 라이브러리 초기화)
 	// 관련 정보가 wsaData에 채워짐
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	// 기본적으로 한국 게임은 TCP이다.
-	// TCP는 통신을 UDP보다 안전하게 해준다.
-
-
-	// af : address Family (AF_INET = IPv4 , AF_INET6 = IPv6)
-	// type : TCP(SOCK_STREAM, vs UDP(SOCK_DGRAM)
-	// protocol : descriptor
-	// return : descriptor
-	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);		// 소문자는 리눅스에서도 사용가능할 수 있음
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
 		HandleError("Socket");
 		return 0;
 	}
 
-	// 연결할 목적지는? (IP 주소 + port) -> xx 아파트 yy호
-	SOCKADDR_IN serverAddr; // IPv4 버젼
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1"); 구식버전
-	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // 현대식 버전
-	serverAddr.sin_port = ::htons(7777);  // 80 : HTTP 특정번호는 규약이 있다.
-	
-	//udp 는 따로 연결이 필요없다.
-
-	// 하지만 즐겨찾기 처럼 내가 보낼 대상을 설정할 수 있다.
-	::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-
-	// -------------------------
-	// 연결 성공 : 이제부터 데이터 송수신 가능!
-
-	cout << "Connected To Server ! " << endl;
-
-	while (true)
-	{
-		//ToDo
-		char sendBuffer[100] = "HelloServer";
-
-
-		// 나의 IP 주소 + 포트 번호 설정 ( 자동 할당) TCP,UDP 상관없음)
-		// Unconnect UDP (기본 UDP)
-	   /*	int32 reslutCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0
-			, (SOCKADDR*)&serverAddr, sizeof(serverAddr));*/
-
-		// Connect UDP
-		
-		for (int32 i = 0; i < 10; ++i)
-		{
-			int32 reslutCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
-			if (reslutCode == SOCKET_ERROR)
-			{
-				HandleError("SendTo");
-				return 0;
-			}
-		}
-
-		
-
-
-		
-		cout << "Send Data Len : " << sizeof(sendBuffer) << endl;
-
-
-		SOCKADDR_IN recvAddr;
-		::memset(&recvAddr, 0, sizeof(recvAddr));
-		int32 addrLen = sizeof(recvAddr);
-		char recvBuffer[1000];
-
-		// Unconnect UDP (기본 UDP) 동작방식은 상관없음
-		/*int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0,
-								(SOCKADDR*)&recvAddr,&addrLen);*/
-		// Connect UDP
-		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-
-
-		if (recvLen <= 0)
-		{
-			int32 errCode = ::WSAGetLastError();
-			cout << "Accept ErrorCode : " << errCode << endl;
-			return 0;
-		}
-
-		cout << "Client Recv Data :  Data : " << recvBuffer << endl;
-		cout << "Client Recv Data :  Len : " << recvLen << endl;
-
-
-		this_thread::sleep_for(1s);
-	}
-
-
-	// ----------------------------
-
-	// 소켓 리소스 반환
 	::closesocket(clientSocket);
 
 	// 윈속 종료
@@ -232,5 +256,3 @@ int main()
 
 
 }
-
-
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -306,10 +306,81 @@ void HandleError(const char* cause)
 
 
 // UDP
+//int main()
+//{
+//	// 윈속 초기화 (ws2_32 라이브러리 초기화)
+//	// 관련 정보가 wsaData에 채워짐
+//	WSAData wsaData;
+//	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+//		return 0;
+//
+//	SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+//	if (serverSocket == INVALID_SOCKET)
+//	{
+//		HandleError("Socket");
+//		return 0;
+//	}
+//
+//
+//	SOCKADDR_IN serverAddr; // IPv4 버젼
+//	::memset(&serverAddr, 0, sizeof(serverAddr));
+//	serverAddr.sin_family = AF_INET;
+//	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+//	//::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // 현대식 버전
+//	serverAddr.sin_port = ::htons(7777);  // 80 : HTTP 특정번호는 규약이 있다.
+//
+//	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+//	{
+//		HandleError("Bind");
+//		return 0;
+//	}
+//
+//	while (true)
+//	{
+//		SOCKADDR_IN clientAddr;
+//		::memset(&clientAddr, 0, sizeof(clientAddr));
+//		int32 addrLen = sizeof(clientAddr);
+//		
+//		this_thread::sleep_for(1s);
+//		
+//		char recvBuffer[1000];
+//
+//		int32 recvLen =  ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0
+//		,(SOCKADDR*)&clientAddr,&addrLen);
+//
+//		if (recvLen <= 0)
+//		{
+//			HandleError("RecvFrom");
+//			return 0;
+//		}
+//
+//		cout << " Recv Data :  Data : " << recvBuffer << endl;
+//		cout << " Recv Data :  Len : " << recvLen << endl; 
+//
+//		int32 errorCode = ::sendto(serverSocket, recvBuffer, recvLen, 0,
+//			(SOCKADDR*)&clientAddr, sizeof(clientAddr));
+//
+//		if (errorCode == SOCKET_ERROR)
+//		{
+//			HandleError("SendTo");
+//			return 0;
+//		}
+//
+//		cout << "Send Data Len : " << sizeof(recvLen) << endl;
+//
+//	}
+//
+//
+//	// 소켓 리소스 반환
+//	::closesocket(serverSocket);
+//
+//	// 윈속 종료
+//	::WSACleanup();
+//}
+
+
 int main()
 {
-	// 윈속 초기화 (ws2_32 라이브러리 초기화)
-	// 관련 정보가 wsaData에 채워짐
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
@@ -321,57 +392,84 @@ int main()
 		return 0;
 	}
 
+	// 옵션을 해석하고 처리할 주체
+	// 소켓 코드 ->SOL_SOCKET
+	// IPv4 -> IPPROTO_IP
+	// TCP 프로토콜 -> INPPROTO_TCP
+	
+	// SO_KEEPALIVE : 주기적으로 연결 상태 확인 여부(TCP Only)
+	//				  상대방이 소리소문없이 연결을 끊는다면?
+	//				  주기적으로 TCP프로토콜 연결 상태확인 -> 끊어진 연결 감지
+	
+	//bool enalbe = true;
+	//::setsockopt(serverSocket,SOL_SOCKET,SO_KEEPALIVE,
+	//	reinterpret_cast<char*>(&enalbe));
+	//
+	// SO_LINGER = 지연하다.
+	// 송신 버퍼에 있는 데이터를 보낼 것인가? 날릴 것인가?
+	
+	// ON_off ==0 이면 closesocket()이 바로 리턴, 아니면 linger 초만큼 대기(default 0)
+	// linger : 대기 시간
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
+	
+	// Half-Close
+	// SD_SEND : send 만 막는다.
+	// SD_RECEIVE : recv 만 막는다.
+	// SD_BOTH : 둘다 막는다.
+	::shutdown(serverSocket, SD_SEND);
+	// 좀 우아하게 닫을 때 
 
-	SOCKADDR_IN serverAddr; // IPv4 버젼
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
-	//::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // 현대식 버전
-	serverAddr.sin_port = ::htons(7777);  // 80 : HTTP 특정번호는 규약이 있다.
+	// SO_SNDBUF = 송신 버퍼 크기
+	 SO_RCVBUF = 수신 버퍼 크기
 
-	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	int32 sendBufferSize;
+	int32 optionLen = sizeof(sendBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufferSize, &optionLen);
+	cout << "송신 버퍼 크기 " << sendBufferSize << endl;
+
+
+	int32 recvBufferSize;
+	::getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)&recvBufferSize, &optionLen);
+	cout << "수신 버퍼 크기 " << recvBufferSize << endl;
+
+
+	// SO_REUSEADDR
+	// IP주소 및 port 재사용
 	{
-		HandleError("Bind");
-		return 0;
+		/*
+		우리가 TCP에서 소켓을 만들어 주소를 만든 후 바인드를 하지만
+			경우에 따라서 그 주소를 따른 곳에서 사용하는 경우가 있다.
+			그래서 남아있는 주소 때문에 바인드가 실패하는 경우가 있어서
+			갖에로 해당 주소를 재사용하겠다고 하는 것이다.
+			*/
+
+		bool enable = true;
+		::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(enable));
 	}
 
-	while (true)
+	
+	// IPPROTO_TCP
+	// TCP_NODELAY : 네이글 알고리즘 작동 여부
+	// 데이터가 충분히 크면 보내고, 그렇지 않으면 데이터가 충분히 쌓일때까지 대기!
+	// 장점 : 작은 패킷이 불필요하게 많이 생성되는 일을 방지
+	// 단점 : 반응 시간 손해
+	// 일반적인 게임에서 끄는 것이 맞다.
 	{
-		SOCKADDR_IN clientAddr;
-		::memset(&clientAddr, 0, sizeof(clientAddr));
-		int32 addrLen = sizeof(clientAddr);
-		
-		this_thread::sleep_for(1s);
-		
-		char recvBuffer[1000];
-
-		int32 recvLen =  ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0
-		,(SOCKADDR*)&clientAddr,&addrLen);
-
-		if (recvLen <= 0)
-		{
-			HandleError("RecvFrom");
-			return 0;
-		}
-
-		cout << " Recv Data :  Data : " << recvBuffer << endl;
-		cout << " Recv Data :  Len : " << recvLen << endl; 
-
-		int32 errorCode = ::sendto(serverSocket, recvBuffer, recvLen, 0,
-			(SOCKADDR*)&clientAddr, sizeof(clientAddr));
-
-		if (errorCode == SOCKET_ERROR)
-		{
-			HandleError("SendTo");
-			return 0;
-		}
-
-		cout << "Send Data Len : " << sizeof(recvLen) << endl;
-
+		bool enable = true;
+		::setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)&enable, sizeof(enable));
 	}
+
+
+
+	//::getsockopt();
+
 
 
 	// 소켓 리소스 반환
+	// send -> closesocket
 	::closesocket(serverSocket);
 
 	// 윈속 종료
